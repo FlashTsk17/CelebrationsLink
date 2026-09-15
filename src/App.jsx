7f11cdb18cd2a1a9b24bb5f1751c96c8d5cd2cb4
Tsadok <adidomahukpeou@gmail.com>
2026-09-15 05:28:13 +0100

    feat: establish CelebrationsLink route architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-  import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import BirthdayLink from './pages/BirthdayLink.jsx'
 import FeteMeres from './pages/FeteMeres.jsx'
+import Organiser from './pages/Organiser.jsx'
+import Celebrer from './pages/Celebrer.jsx'
+import ComingSoon from './pages/ComingSoon.jsx'
+import './styles/app.css'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/organiser" element={<Organiser />} />
+        <Route path="/organiser/type" element={<ComingSoon title="Choisir le type d’événement" />} />
+        <Route path="/organiser/annonce" element={<ComingSoon title="Créer une annonce" />} />
+        <Route path="/organiser/invitation" element={<ComingSoon title="Créer une invitation" />} />
+        <Route path="/celebrer" element={<Celebrer />} />
+        <Route path="/celebrer/type" element={<ComingSoon title="Choisir une occasion" />} />
+        <Route path="/celebrer/personnaliser" element={<ComingSoon title="Personnaliser ton vœu" />} />
+        <Route path="/e/:slug" element={<ComingSoon title="Événement" />} />
+        <Route path="/c/:slug" element={<ComingSoon title="Célébration" />} />
         <Route path="/birthday" element={<BirthdayLink />} />
         <Route path="/fete-meres" element={<FeteMeres />} />
       </Routes>
