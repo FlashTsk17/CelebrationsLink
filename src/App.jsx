32ce541cbd3810258222269814a7fa5927469003
Tsadok <adidomahukpeou@gmail.com>
2026-09-15 05:37:52 +0100

    feat: connect organizer dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Organiser from './pages/Organiser.jsx'
 import OrganiserType from './pages/OrganiserType.jsx'
 import EventForm from './pages/EventForm.jsx'
 import PublicEvent from './pages/PublicEvent.jsx'
+import OrganiserDashboard from './pages/OrganiserDashboard.jsx'
 import Celebrer from './pages/Celebrer.jsx'
 import ComingSoon from './pages/ComingSoon.jsx'
 import './styles/app.css'
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/organiser/type" element={<OrganiserType />} />
         <Route path="/organiser/annonce" element={<EventForm mode="announcement" />} />
         <Route path="/organiser/invitation" element={<EventForm mode="invitation" />} />
+        <Route path="/organiser/dashboard" element={<OrganiserDashboard />} />
         <Route path="/celebrer" element={<Celebrer />} />
         <Route path="/celebrer/type" element={<ComingSoon title="Choisir une occasion" />} />
         <Route path="/celebrer/personnaliser" element={<ComingSoon title="Personnaliser ton vœu" />} />
