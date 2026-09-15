57c21531ac2edd7ee6efe0ddd779ec468567a9ec
Tsadok <adidomahukpeou@gmail.com>
2026-09-15 05:31:15 +0100

    feat: wire real event creation routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from './pages/Home.jsx'
 import BirthdayLink from './pages/BirthdayLink.jsx'
 import FeteMeres from './pages/FeteMeres.jsx'
 import Organiser from './pages/Organiser.jsx'
+import OrganiserType from './pages/OrganiserType.jsx'
+import EventForm from './pages/EventForm.jsx'
+import PublicEvent from './pages/PublicEvent.jsx'
 import Celebrer from './pages/Celebrer.jsx'
 import ComingSoon from './pages/ComingSoon.jsx'
 import './styles/app.css'
@@ -13,13 +16,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/organiser" element={<Organiser />} />
-        <Route path="/organiser/type" element={<ComingSoon title="Choisir le type d’événement" />} />
-        <Route path="/organiser/annonce" element={<ComingSoon title="Créer une annonce" />} />
-        <Route path="/organiser/invitation" element={<ComingSoon title="Créer une invitation" />} />
+        <Route path="/organiser/type" element={<OrganiserType />} />
+        <Route path="/organiser/annonce" element={<EventForm mode="announcement" />} />
+        <Route path="/organiser/invitation" element={<EventForm mode="invitation" />} />
         <Route path="/celebrer" element={<Celebrer />} />
         <Route path="/celebrer/type" element={<ComingSoon title="Choisir une occasion" />} />
         <Route path="/celebrer/personnaliser" element={<ComingSoon title="Personnaliser ton vœu" />} />
-        <Route path="/e/:slug" element={<ComingSoon title="Événement" />} />
+        <Route path="/e/:slug" element={<PublicEvent />} />
         <Route path="/c/:slug" element={<ComingSoon title="Célébration" />} />
         <Route path="/birthday" element={<BirthdayLink />} />
         <Route path="/fete-meres" element={<FeteMeres />} />
